a831a1d11345f609c522c3e9771f0c1469b20fd7
Hussam-22 <hussam@hotmail.co.uk>
2024-05-27 11:55:19 +0400

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,15 @@
-import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
-import mdx from "@astrojs/mdx";
-import sitemap from "@astrojs/sitemap";
-import icon from "astro-icon";
+import { defineConfig } from 'astro/config'
+import tailwind from '@astrojs/tailwind'
+import mdx from '@astrojs/mdx'
+import sitemap from '@astrojs/sitemap'
+import icon from 'astro-icon'
+// import iconify from 'astro-iconify'
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://astroship.web3templates.com",
-  integrations: [tailwind(), mdx(), sitemap(), icon()],
-});
+	site: 'https://astroship.web3templates.com',
+	integrations: [tailwind(), mdx(), sitemap(), icon()]
+	// devOptions: {
+	// 	hmr: true
+	// }
+})
