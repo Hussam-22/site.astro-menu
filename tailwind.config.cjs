0e1397f5f522a593a42bdc33f99112dbf5d9b28e
Hussam-22 <hussam@hotmail.co.uk>
2024-05-27 15:26:14 +0400

    adding tailwind animation

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,18 +1,22 @@
 /** @type {import('tailwindcss').Config} */
-const defaultTheme = require("tailwindcss/defaultTheme");
+const defaultTheme = require('tailwindcss/defaultTheme')
 module.exports = {
-  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
-  theme: {
-    extend: {
-      fontFamily: {
-        sans: [
-          "Bricolage Grotesque Variable",
-          "Inter Variable",
-          "Inter",
-          ...defaultTheme.fontFamily.sans,
-        ],
-      },
-    },
-  },
-  plugins: [require("@tailwindcss/typography")],
-};
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {
+			fontFamily: {
+				sans: [
+					'Bricolage Grotesque Variable',
+					'Inter Variable',
+					'Inter',
+					...defaultTheme.fontFamily.sans
+				]
+			}
+		}
+	},
+	plugins: [
+		require('@tailwindcss/typography'),
+		require('tailwindcss-animated'),
+		require('tailwindcss-intersect')
+	]
+}
