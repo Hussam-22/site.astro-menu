7291820c82f5846cb6436b70ccd4b1999f7ec402
Hussam-22 <hussam@hotmail.co.uk>
2024-05-31 00:21:49 +0400

    update

---
+++ src/content/features.js
@@ -0,0 +1,51 @@
+export const FEATURES = [
+	{
+		title: 'Dynamic QR Code Menu',
+		description:
+			"Create a dynamic QR code menu for your restaurant, or cafe, Update your menu in real-time and let your customers scan the QR code to view it. It's that simple!",
+		icon: 'qr-code'
+	},
+	{
+		title: 'Self Order',
+		description:
+			'Allow your customers to take their orders themselves select portions and add notes to their orders. This will help you reduce the number of waiters you need, and increase the speed of service',
+		icon: 'self-order',
+		video: 'self-order.mp4'
+	},
+	{
+		title: 'Waiter(ess)/Kitchen Dashboard',
+		description:
+			'Get a dashboard for your waiters to manage orders, and a dashboard for your kitchen to manage the orders and notify the waiter when the order is ready.',
+		icon: 'waiter'
+	},
+	{
+		title: 'Analytics',
+		description:
+			'Get insights on your customers, and orders. Know what your customers like, and what they do not like. Improve your menu based on the data you get.',
+		icon: 'analytics'
+	},
+	{
+		title: 'Auto Translation',
+		description:
+			'Dont let the language be a barrier between you and your customers. Astro Menu will automatically translate your menu to your most frequent customers languages, and you can always add more languages.',
+		icon: 'translate'
+	},
+	{
+		title: 'Simple and Fast Menu',
+		description:
+			'With Astro Menu, you can create a simple and fast menu for your restaurant. It is easy to use and fast to load., dont overload customer with larg pdf files or images that takes forver to load.',
+		icon: 'fast'
+	},
+	{
+		title: 'Minimum Clicks design in mind',
+		description:
+			'Astro-Menu was design with minimum clicks in mind, we want to make sure that your customers can order with the least number of clicks possible without overwhelming them with too many options.',
+		icon: 'min-clicks'
+	},
+	{
+		title: 'Wifi Password from the menu',
+		description:
+			'You can add your wifi password to the menu, so your customers can easily connect to your wifi without asking your staff for the password.',
+		icon: 'wifi'
+	}
+]
---
+++ src/content/features/feature-1.md
@@ -0,0 +1,7 @@
+---
+slug: this is feature number 1
+isFinal: true
+features: ['1', 2, 'hussam', false]
+---
+
+lorem some very long text, actually its very short
