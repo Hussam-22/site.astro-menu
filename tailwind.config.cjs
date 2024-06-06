061622de370540e28921f47c19a47407d88302e4
Hussam-22 <hussam@hotmail.co.uk>
2024-06-06 15:26:44 +0400

    fix mobile style

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,4 +1,3 @@
-/** @type {import('tailwindcss').Config} */
 const defaultTheme = require('tailwindcss/defaultTheme')
 module.exports = {
 	darkMode: 'selector',
@@ -15,36 +14,9 @@ module.exports = {
 			}
 		}
 	},
-	daisyui: {
-		themes: [
-			{
-				cupcake: {
-					...require('daisyui/src/theming/themes')['cupcake'],
-					'base-100': '#FFFFFF',
-					'base-content': '#000000',
-					'--rounded-btn': '0.5rem',
-					primary: '#FB7185',
-					secondary: '#000000',
-					'.btn-primary': { color: '#FFFFFF' },
-					'.btn-secondary': { color: '#FFFFFF', backgroundColor: '#000000' }
-				}
-			},
-			{
-				business: {
-					...require('daisyui/src/theming/themes')['business'],
-					primary: '#FB7185',
-					secondary: '#000000',
-					'--rounded-btn': '0.5rem',
-					'.btn-primary': { color: '#FFFFFF' },
-					'.btn-secondary': { color: '#000000', backgroundColor: '#FFFFFF' }
-				}
-			}
-		]
-	},
 	plugins: [
 		require('@tailwindcss/typography'),
 		require('tailwindcss-animated'),
-		require('tailwindcss-intersect'),
-		require('daisyui')
+		require('tailwindcss-intersect')
 	]
 }
