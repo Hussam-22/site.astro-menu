d13503cbce3bddf8cf9013450328d5e5af315a02
Hussam-22 <hussam@hotmail.co.uk>
2024-06-26 00:33:51 +0400

    remove iconify and replace with normal img svg

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,6 @@ import tailwind from '@astrojs/tailwind'
 import mdx from '@astrojs/mdx'
 import sitemap from '@astrojs/sitemap'
 import icon from 'astro-icon'
-// import iconify from 'astro-iconify'
 
 // https://astro.build/config
 export default defineConfig({
