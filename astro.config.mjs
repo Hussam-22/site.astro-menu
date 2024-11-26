59463c5a442365adeeb7f877f006afd0ebf90a91
Hussam-22 <hussam@hotmail.co.uk>
2024-11-26 14:54:55 +0400

    add favicon and update qr code image

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import react from '@astrojs/react'
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://astroship.web3templates.com',
+	site: 'https://astro-menu.vercel.app/',
 	integrations: [tailwind(), mdx(), sitemap(), icon(), react()],
 	server: {
 		host: true
