3c4ff44ff2480c82acc6348c1cff41c420fe22db
Hussam Al Khudari <hussam@hotmail.co.uk>
2025-08-01 19:26:07 +0400

    update

--- src/components/ui/side-tabs.jsx
+++ src/components/ui/side-tabs.jsx
@@ -7,17 +7,18 @@ export default function SideTabs({ tabs }) {
     <div className="bg-base-200 rounded-4xl p-4 flex flex-row gap-4">
       <div className="w-4/12 flex flex-col gap-2 py-4">
         {tabs.map((tab, index) => (
-          <button
+          <div
             key={index}
-            className={`w-full text-center text-xl p-2 rounded-4xl  ${
+            className={`w-full text-center text-xl p-2 rounded-4xl flex flex-row items-center gap-4 px-4  ${
               activeTabIndex === index
                 ? "bg-primary text-white font-bold"
                 : "bg-base-100"
             }`}
             onClick={() => setActiveTabIndex(index)}
           >
-            {tab.label}
-          </button>
+            <img src="src/assets/icons/cafe.svg" className="w-8 h-8" />
+            <button key={index}>{tab.label}</button>
+          </div>
         ))}
       </div>
       <div className="w-8/12 bg-base-100 p-4 rounded-4xl"></div>
